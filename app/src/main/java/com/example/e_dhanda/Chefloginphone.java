d81dde57ea6204d607e8f69a61c04ecb78fea0a4
Ayush Jain <jainayush587@gmail.com>
2020-12-03 12:13:19 +0530

    Chef login, verification, auto sign-in

--- app/src/main/java/com/example/e_dhanda/Chefloginphone.java
+++ app/src/main/java/com/example/e_dhanda/Chefloginphone.java
@@ -2,13 +2,66 @@ package com.example.e_dhanda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.hbb20.CountryCodePicker;
 
 public class Chefloginphone extends AppCompatActivity {
+
+    EditText num;
+    Button sendotp,signinemail;
+    TextView signup;
+    CountryCodePicker cpp;
+    FirebaseAuth Fauth;
+    String number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chefloginphone);
+
+        num = (EditText)findViewById(R.id.CPhoneNo);
+        sendotp = (Button)findViewById(R.id.COtp);
+        cpp=(CountryCodePicker)findViewById(R.id.CountryCode);
+        signinemail=(Button)findViewById(R.id.ChefEmailBtn);
+        signup = (TextView)findViewById(R.id.CCreateAcc);
+
+        Fauth = FirebaseAuth.getInstance();
+
+        sendotp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                number=num.getText().toString().trim();
+                String Phonenum = cpp.getSelectedCountryCodeWithPlus()+number;
+                Intent b = new Intent(Chefloginphone.this,Chefsendotp.class);
+
+                b.putExtra("Phonenum",Phonenum);
+                startActivity(b);
+                finish();
+
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Chefloginphone.this,ChefRegistration.class));
+                finish();
+            }
+        });
+        signinemail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Chefloginphone.this,Cheflogin.class));
+                finish();
+            }
+        });
+
     }
 }
