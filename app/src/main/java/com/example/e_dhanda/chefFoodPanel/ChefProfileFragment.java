8ee1ca5ef4974251cd276e67a4a253e4cf05090c
Ayush Jain <jainayush587@gmail.com>
2020-12-04 18:31:54 +0530

    Delivery Done

--- app/src/main/java/com/example/e_dhanda/chefFoodPanel/ChefProfileFragment.java
+++ app/src/main/java/com/example/e_dhanda/chefFoodPanel/ChefProfileFragment.java
@@ -1,23 +1,39 @@
 package com.example.e_dhanda.chefFoodPanel;
 
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import com.example.e_dhanda.R;
 
 public class ChefProfileFragment extends Fragment {
+
+    Button postDish;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_chef_profile,null);
         getActivity().setTitle("Post Dish");
+
+        postDish =  (Button)v.findViewById(R.id.post_dish);
+
+        postDish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(),chef_postDish.class));
+            }
+        });
         return v;
     }
 }
---
+++ app/src/main/java/com/example/e_dhanda/chefFoodPanel/chef_postDish.java
@@ -0,0 +1,16 @@
+package com.example.e_dhanda.chefFoodPanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.e_dhanda.R;
+
+public class chef_postDish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_post_dish);
+    }
+}
