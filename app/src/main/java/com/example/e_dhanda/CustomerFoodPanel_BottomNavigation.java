a8fd20181cfd30e5346dc8a277c01d6654425d25
Ayush Jain <jainayush587@gmail.com>
2020-12-06 14:14:41 +0530

    Delivery Done

--- app/src/main/java/com/example/e_dhanda/CustomerFoodPanel_BottomNavigation.java
+++ app/src/main/java/com/example/e_dhanda/CustomerFoodPanel_BottomNavigation.java
@@ -1,14 +1,72 @@
 package com.example.e_dhanda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity {
+import com.example.e_dhanda.chefFoodPanel.ChefHomeFragment;
+import com.example.e_dhanda.chefFoodPanel.ChefOrderFragment;
+import com.example.e_dhanda.chefFoodPanel.ChefPendingOrderFragment;
+import com.example.e_dhanda.chefFoodPanel.ChefProfileFragment;
+import com.example.e_dhanda.customerFoodPanel.CustomerCartFragment;
+import com.example.e_dhanda.customerFoodPanel.CustomerHomeFragment;
+import com.example.e_dhanda.customerFoodPanel.CustomerOrdersFragment;
+import com.example.e_dhanda.customerFoodPanel.CustomerProfileFragment;
+import com.example.e_dhanda.customerFoodPanel.CustomerTrackFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class CustomerFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_food_panel__bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.cust_Home:
+                fragment=new CustomerHomeFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cart:
+                fragment=new CustomerCartFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.cust_profile:
+                fragment=new CustomerProfileFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.Cust_order:
+                fragment=new CustomerOrdersFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.track:
+                fragment=new CustomerTrackFragment();
+                break;
+        }
+        return loadcustomerfragment(fragment);
+
+    }
+
+    private boolean loadcustomerfragment(Fragment fragment) {
+
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
--- app/src/main/java/com/example/e_dhanda/DeliveryFoodPanel_BottomNavigation.java
+++ app/src/main/java/com/example/e_dhanda/DeliveryFoodPanel_BottomNavigation.java
@@ -1,14 +1,51 @@
 package com.example.e_dhanda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity {
+import com.example.e_dhanda.customerFoodPanel.CustomerCartFragment;
+import com.example.e_dhanda.customerFoodPanel.CustomerHomeFragment;
+import com.example.e_dhanda.deliveryFoodPanel.DeliveryPendingOrderFragment;
+import com.example.e_dhanda.deliveryFoodPanel.DeliveryShipOrderFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class DeliveryFoodPanel_BottomNavigation extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_food_panel__bottom_navigation);
+        BottomNavigationView navigationView = findViewById(R.id.delivery_bottom_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.shiporders:
+                fragment=new DeliveryShipOrderFragment();
+                break;
+        }
+        switch (item.getItemId()){
+            case R.id.pendingorders:
+                fragment=new DeliveryPendingOrderFragment();
+                break;
+        }
+        return loaddeliveryfragment(fragment);
+    }
+
+    private boolean loaddeliveryfragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            return true;
+        }
+        return false;
     }
 }
